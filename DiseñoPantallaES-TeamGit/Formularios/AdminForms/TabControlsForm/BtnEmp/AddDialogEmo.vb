14a95d771d7693a93dd0894b8733617ea9d49a4f
leocnavarrete3@gmail.com <leocnavarrete3@gmail.com>
2022-05-22 14:22:10 -0600

    Added a form AddDialogEmp

---
+++ DiseñoPantallaES-TeamGit/Formularios/AdminForms/TabControlsForm/BtnEmp/AddDialogEmo.vb
@@ -0,0 +1,16 @@
+﻿Public Class AddDialogEmp
+
+    Dim BDEmpleado As New BDSistemaEySDataSetTableAdapters.EmpleadoTableAdapter
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim idEmp As Integer
+
+        idEmp = Convert.ToInt32(idTxt.Text)
+
+        BDEmpleado.InsertEmpleado(idEmp, nombreTxt.Text, segNombreTxt.Text, apellidoTxt.Text, segApellidoTxt.Text, passwordTxt.Text)
+
+        MsgBox("Guardado")
+
+    End Sub
+End Class
